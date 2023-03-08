4391ef633c6bb0a9156f89827950794e7bdf174b
Charles Affouard <charles.affouard@epitech.eu>
2023-03-08 22:34:06 +0100

    Init Repo

---
+++ palmhelp/android/app/src/main/kotlin/com/example/palmhelp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.palmhelp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ palmhelp/lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () {
+              
+            },
+            child: const Text("Hello"),
+          )
+        ],
+      ),
+    );
+  }
+}
