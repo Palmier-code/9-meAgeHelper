7121b1caffc9b1fd54c0f020890c51a062d5d520
Charles Affouard <charles.affouard@epitech.eu>
2023-09-19 12:36:42 +0200

    [+] add the recktof the army

--- lib/army/display_army.dart
+++ lib/army/display_army.dart
@@ -10,6 +10,15 @@ class DisplayArmy extends StatefulWidget {
 }
 
 class _DisplayArmyState extends State<DisplayArmy> {
+  int total = 0;
+  late ValueNotifier<List<bool>> widgetListCheck;
+
+  @override
+  void initState() {
+    super.initState();
+    widgetListCheck = ValueNotifier(List.generate(20, (index) => false));
+  }
+
   @override
   Widget build(BuildContext context) {
     List<String> armySplit = widget.army.split('\n');
@@ -17,25 +26,60 @@ class _DisplayArmyState extends State<DisplayArmy> {
       padding: const EdgeInsets.all(8),
       itemCount: armySplit.length,
       itemBuilder: (BuildContext context, int index) {
-        return armyContainer(armySplit[index], index, armySplit.length);
+        return armyContainer(
+          armySplit[index],
+          index,
+          armySplit.length,
+          widgetListCheck.value[index],
+        );
       },
     );
   }
 
-  Widget armyContainer(String unity, int index, int max) {
-    if (index == 0 || index == max) {
+  Widget armyContainer(String unity, int index, int max, bool widgetCheck) {
+    // TextDecoration tmp = widgetListCheck.value[index] == false
+    //     ? TextDecoration.none
+    //     : TextDecoration.lineThrough;
+
+    if (index == 0) {
       return Center(
         child: Text(
           unity,
         ),
       );
+    } else if (index == max - 1) {
+      return totalPoint(total);
     }
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
-        child: Text(unity),
-        onTap: () {},
+        splashColor: Colors.black.withOpacity(0.8),
+        child: Text(
+          unity,
+          style: TextStyle(
+            decoration: widgetListCheck.value[index] == false
+                ? TextDecoration.none
+                : TextDecoration.lineThrough,
+          ),
+        ),
+        onTap: () {
+          setState(() {
+            print(widgetListCheck.value[index]);
+            widgetListCheck.value[index] = !widgetListCheck.value[index];
+            print(widgetListCheck.value[index]);
+            total = widgetListCheck.value[index]
+                ? total + int.parse(unity.substring(0, 3))
+                : total - int.parse(unity.substring(0, 3));
+          });
+        },
       ),
+    );
+  }
+
+  Widget totalPoint(int total) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text("Total points perdu: $total"),
     );
   }
 }
--- lib/credential_screen.dart
+++ lib/credential_screen.dart
@@ -55,12 +55,12 @@ class _CredentialScreenState extends State<CredentialScreen> {
             height: 24,
           ),
           Text(
-            "App Version: alpha 1.2.2",
+            "App Version: alpha 1.3.0",
             style: TextStyle(fontSize: 18),
             textAlign: TextAlign.center,
           ),
           Text(
-            "Changement pour cette version: \n-Ajout de la page armée\n-Ajout du stockage de l'armée\n-Ajout du début des calculs de fin de partie",
+            "Changement pour cette version: \n-Ajout de la page armée\n-Ajout du stockage de l'armée\n-Ajout du début des calculs de fin de partie\n-Ajout du alcul des points de vos armées",
             style: TextStyle(fontSize: 18),
             textAlign: TextAlign.center,
           )
